014f9b268452a587ab3d1efd5139f15b6e4354dd
Trup10ka <lukys3036@gmail.com>
2024-03-03 18:39:10 +0100

    Implement toString for Config

--- DbProject/Config/Data/Config.cs
+++ DbProject/Config/Data/Config.cs
@@ -6,4 +6,14 @@ public record Config(
     string Password,
     short? Port = null,
     string? DbName = null
-);
+)
+{
+    public override string ToString()
+    {
+        return "Host: " + Host + "\n" +
+               "Username: " + Username + "\n" +
+               "Password: " + Password + "\n" +
+               "Port: " + Port + "\n" +
+               "DbName: " + DbName + "\n";
+    }
+}
