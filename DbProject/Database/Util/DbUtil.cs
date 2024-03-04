d3544608ee5379c0f973126c0cc2fdfc8cf57c1e
Trup10ka <lukys3036@gmail.com>
2024-03-04 08:33:02 +0100

    Implement util class for db

---
+++ DbProject/Database/Util/DbUtil.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Configuration = DbProject.Config.Data.Config;
+
+namespace DbProject.Database.Util;
+
+public static class DbUtil
+{
+    public static string AssembleConnectionString(Configuration config)
+    {
+        var builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = config.Host,
+            UserID = config.Username,
+            Password = config.Password,
+            TrustServerCertificate = true
+        };
+        
+        return builder.ConnectionString;
+    }
+}
