c104be3b189d807f5a27542a8f0b3137b7e5febe
Trup10ka <lukys3036@gmail.com>
2024-03-07 23:20:32 +0100

    Implement struct column

---
+++ DbProject/Database/Data/Column.cs
@@ -0,0 +1,11 @@
+﻿namespace DbProject.Database.Data;
+
+public readonly struct Column(
+    string name, 
+    ColumnType type
+    ) 
+{
+    public string Name { get; } = name;
+    
+    public ColumnType Type { get; init; } = type;
+}
---
+++ DbProject/Database/Data/ColumnType.cs
@@ -0,0 +1,9 @@
+﻿namespace DbProject.Database.Data;
+
+public enum ColumnType
+{
+    VARCHAR,
+    INT,
+    DATE,
+    BIT
+}
