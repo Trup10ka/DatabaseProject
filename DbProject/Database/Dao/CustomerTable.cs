af642f0303e54dc6710af6c891ff6bd72a871b1c
Trup10ka <lukys3036@gmail.com>
2024-03-07 23:21:39 +0100

    Implement CustomerTable

---
+++ DbProject/Database/Dao/CustomerTable.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DbProject.Database.Dao;
+
+public class CustomerTable : Table
+{
+    public CustomerTable() : base("customer")
+    {
+        Varchar("name");
+        Varchar("surname");
+        Bit("gender");
+    }
+}
