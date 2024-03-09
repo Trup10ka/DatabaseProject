ec47942ddd141d9f373243271cde8be3a872f68d
Trup10ka <lukys3036@gmail.com>
2024-03-09 22:08:51 +0100

    Implement InsertQuery

--- DbProject/Database/Query/InsertQuery.cs
+++ DbProject/Database/Query/InsertQuery.cs
@@ -1,11 +1,24 @@
-﻿namespace DbProject.Database.Query;
+﻿using DbProject.Database.Data;
+using Microsoft.Data.SqlClient;
+using static DbProject.Database.ConnectionManager;
+
+namespace DbProject.Database.Query;
 
 public class InsertQuery(
     string tableName,
     string[] columnNames,
     string[] values
-    ) : RawQuery(tableName)
+    ) : AbstractQuery(tableName)
 {
     public override string SqlCommand => 
         $"INSERT INTO {TableName} ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", values)})";
+    
+    public override List<ResultRow> ExecuteQuery()
+    {
+        using var connection = CreateConnection();
+        using var command = new SqlCommand(SqlCommand, connection);
+        var rowsAffected = command.ExecuteNonQuery();
+        
+        return rowsAffected >= 1 ? [ new ResultRow(RowsAffected: rowsAffected)] : [ new ResultRow(RowsAffected: 0)];
+    }
 }
