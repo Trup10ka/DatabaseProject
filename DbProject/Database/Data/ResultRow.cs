bb6e36d79cd4fa72ff871d6d6ca3c091ef78cd9d
Trup10ka <lukys3036@gmail.com>
2024-03-09 22:04:32 +0100

    Implement result row record

---
+++ DbProject/Database/Data/ResultRow.cs
@@ -0,0 +1,12 @@
+﻿namespace DbProject.Database.Data;
+
+public record ResultRow(
+    List<string>? ColumnValue = null,
+    int RowsAffected = 0
+)
+{
+    public override string ToString()
+    {
+        return ColumnValue != null ? $"{string.Join(", ", ColumnValue)}, RowsAffected: {RowsAffected}" : $"RowsAffected: {RowsAffected}";
+    }
+}
