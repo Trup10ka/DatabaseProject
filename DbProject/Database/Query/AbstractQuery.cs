c9017ada45ddfd5f113c42fbf26f37615405688c
Trup10ka <lukys3036@gmail.com>
2024-03-09 22:04:19 +0100

    Remove IQuery and Change "RawQuery" to "AbstractQuery"

---
+++ DbProject/Database/Query/AbstractQuery.cs
@@ -0,0 +1,12 @@
+﻿using DbProject.Database.Data;
+
+namespace DbProject.Database.Query;
+
+public abstract class AbstractQuery(string tableName)
+{
+    protected string TableName { get; } = tableName;
+    
+    public abstract string SqlCommand { get; }
+
+    public abstract List<ResultRow> ExecuteQuery();
+}
