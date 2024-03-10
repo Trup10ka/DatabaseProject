f6b8d08551aeece24bc4ece7683d4031d76e3ac8
Trup10ka <lukys3036@gmail.com>
2024-03-10 22:00:00 +0100

    Implement general util class

---
+++ DbProject/Database/Util/Util.cs
@@ -0,0 +1,25 @@
+﻿using DbProject.Database.Query;
+
+namespace DbProject.Database.Util;
+
+public static class Util
+{
+    public static int GetNumberOfEmployees()
+    {
+        var selectQuery = new SelectQuery("employee", ["COUNT(*)"]);
+        return int.Parse(selectQuery.ExecuteQuery()[0].ColumnValue![0]);
+    }
+    
+    public static int ResolvePaymentMethod(string paymentMethod)
+    {
+        return paymentMethod.ToLower() switch
+        {
+            "cash" => 1,
+            "credit card" => 2,
+            "debit card" => 3,
+            "pay pal" => 4,
+            "google pay" => 5,
+            _ => 0
+        };
+    }
+}
