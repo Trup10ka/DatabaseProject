a838cf2a799a913ed1ea60b6d99b48b97b4d721d
Trup10ka <lukys3036@gmail.com>
2024-03-10 21:58:48 +0100

    Update InsertQuery

--- DbProject/Database/Query/InsertQuery.cs
+++ DbProject/Database/Query/InsertQuery.cs
@@ -1,6 +1,6 @@
 ﻿using DbProject.Database.Data;
 using Microsoft.Data.SqlClient;
-using static DbProject.Database.ConnectionManager;
+using static DbProject.Database.Util.ConnectionManager;
 
 namespace DbProject.Database.Query;
 
@@ -11,14 +11,45 @@ public class InsertQuery(
     ) : AbstractQuery(tableName)
 {
     public override string SqlCommand => 
-        $"INSERT INTO {TableName} ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", values)})";
+        $"INSERT INTO {TableName} ({string.Join(", ", columnNames)}) VALUES ({AssembleValues(values)})";
     
     public override List<ResultRow> ExecuteQuery()
     {
         using var connection = CreateConnection();
-        using var command = new SqlCommand(SqlCommand, connection);
-        var rowsAffected = command.ExecuteNonQuery();
+        connection.Open();
+        var command = new SqlCommand(SqlCommand, connection);
+        var rowsAffected = 0;
+        try
+        {
+           rowsAffected = command.ExecuteNonQuery(); 
+        }
+        catch (SqlException e)
+        {
+            Console.Error.WriteLine(e.Message);
+        }
         
         return rowsAffected >= 1 ? [ new ResultRow(RowsAffected: rowsAffected)] : [ new ResultRow(RowsAffected: 0)];
+    }
+    
+    private static string AssembleValues(IReadOnlyList<string> valuesArray)
+    {
+        var result = "";
+        for (var i = 0; i < valuesArray.Count; i++)
+        {
+            if (int.TryParse(valuesArray[i], out var valueInt))
+            {
+                result += valueInt;
+                continue;
+            }
+
+            if (decimal.TryParse(valuesArray[i], out var valueDecimal))
+            {
+                result += valueDecimal;
+                continue;
+            }
+            result += $"'{valuesArray[i]}'";
+            if (i != valuesArray.Count - 1) result += ", ";
+        }
+        return result;
     }
 }
